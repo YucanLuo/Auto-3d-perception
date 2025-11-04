25a035701c1bbd1efcac40bb1fd20ceac8f70240
OvomaltineX <84125206+YucanLuo@users.noreply.github.com>
2025-11-04 21:01:50 +0100

    pt3 environment updated

--- scripts/run_jupyter.sh
+++ scripts/run_jupyter.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
+source /opt/conda/etc/profile.d/conda.sh
+conda activate pt3d
 python -m ipykernel install --user --name=pt3d || true
 exec jupyter lab \
   --ip=0.0.0.0 --port=8888 --no-browser \
